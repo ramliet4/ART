5a18a8aeb796be0b7fca5d28c6130cb50dbf1b9c
meakinl2 <lewismeakin1614@gmail.com>
2026-03-21 14:47:54 +0000

    21/03/26 Art board basic structure set up.

--- main.py
+++ main.py
@@ -6,25 +6,5 @@ app = Flask(__name__)
 def Main():
     return render_template("homepage.html")
 
-# @app.route("/experience")
-# def Experience():
-#     return render_template("experience.html")
-
-# @app.route("/certifications")
-# def Certifications():
-#     return render_template("certifications.html")
-
-# @app.route("/projects")
-# def Projects():
-#     return render_template("projects.html")
-
-# @app.route("/contact")
-# def Contact():
-#     return render_template("contact.html")
-
-# @app.route("/playground")
-# def Playground():
-#     return render_template("playground.html")
-
 if __name__ == "__main__":
     app.run(debug=True)
